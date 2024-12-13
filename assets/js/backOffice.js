e558fef7b24a5b3e7c19bb9a2cd24955745e0359
Domy15 <domenico2016dellaventura@gmail.com>
2024-12-13 14:42:29 +0100

    commit

---
+++ assets/js/backOffice.js
@@ -0,0 +1,114 @@
+const h1 = document.getElementById("h1");
+const form = document.getElementById("form");
+const productName = document.getElementById("name");
+const productBrand = document.getElementById("brand");
+const imgUrl = document.getElementById("img");
+const productPrice = document.getElementById("price");
+const productDescription = document.getElementById("description");
+const url = "https://striveschool-api.herokuapp.com/api/product/";
+const id = sessionStorage.getItem("id");
+const btnClear = document.querySelector(".clear");
+
+document.addEventListener("load", init());
+
+function init() {
+    if(id){
+        getData();
+    }
+}
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    if (!id) {
+        postData();
+    } else {
+        putData();
+    }
+});
+
+btnClear.addEventListener("click", function () {
+    sessionStorage.clear();
+});
+
+async function postData() {
+    try {
+        const requestBody = {
+            name: productName.value,
+            brand: productBrand.value,
+            imageUrl: imgUrl.value,
+            price: parseInt(productPrice.value),
+            description: productDescription.value,
+        };
+        console.log("Dati inviati:", requestBody);
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzVjMDAxY2QyMjA3MTAwMTVkZTJmNTUiLCJpYXQiOjE3MzQwODI1ODksImV4cCI6MTczNTI5MjE4OX0.O7LIgG48tut19TZkLFvlRfJ4pvxK7FrXuGb5K41KB-g",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(requestBody),
+        });
+        const result = await response.json();
+        if (!response.ok) {
+            console.error("Errore nel server:", result);
+        } else {
+            h1.innerText = "Aggiungi";
+            console.log("Prodotto aggiunto con successo:", result);
+        }
+    } catch (error) {
+        console.error("Errore nella richiesta:", error);
+    }
+}
+
+async function getData() {
+    try {
+        const response = await fetch(url + id, {
+            method: "GET",
+            headers: {
+                Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzVjMDAxY2QyMjA3MTAwMTVkZTJmNTUiLCJpYXQiOjE3MzQwODI1ODksImV4cCI6MTczNTI5MjE4OX0.O7LIgG48tut19TZkLFvlRfJ4pvxK7FrXuGb5K41KB-g",
+            },
+        });
+        const result = await response.json();
+        if (response.ok) {
+            productName.value = result.name;
+            productBrand.value = result.brand;
+            imgUrl.value = result.imageUrl;
+            productPrice.value = result.price;
+            productDescription.value = result.description;
+            h1.innerHTML = `Modifica prodotto: ${result.name}`;
+        } else {
+            console.error("Errore nel recupero del prodotto:", result);
+        }
+    } catch (error) {
+        console.error("Errore nella richiesta GET:", error);
+    }
+}
+
+async function putData() {
+    try {
+        const requestBody = {
+            name: productName.value,
+            brand: productBrand.value,
+            imageUrl: imgUrl.value,
+            price: parseInt(productPrice.value),
+            description: productDescription.value,
+        };
+        console.log("Dati inviati:", requestBody);
+        const response = await fetch(url + id, {
+            method: "PUT",
+            headers: {
+                Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzVjMDAxY2QyMjA3MTAwMTVkZTJmNTUiLCJpYXQiOjE3MzQwODI1ODksImV4cCI6MTczNTI5MjE4OX0.O7LIgG48tut19TZkLFvlRfJ4pvxK7FrXuGb5K41KB-g",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(requestBody),
+        });
+        const result = await response.json();
+        if (!response.ok) {
+            console.error("Errore nel server:", result);
+        } else {
+            console.log("Prodotto aggiornato con successo:", result);
+        }
+    } catch (error) {
+        console.error("Errore nella richiesta:", error);
+    }
+}
---
+++ assets/js/script.js
@@ -0,0 +1,47 @@
+const url = "https://striveschool-api.herokuapp.com/api/product/";
+const div = document.querySelector(".row");
+
+document.addEventListener("laod", init());
+
+function init() {
+    getData();
+}
+
+async function getData() {
+    try {
+        const response = await fetch(url,
+            {
+                headers: {
+                    Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzVjMDAxY2QyMjA3MTAwMTVkZTJmNTUiLCJpYXQiOjE3MzQwODI1ODksImV4cCI6MTczNTI5MjE4OX0.O7LIgG48tut19TZkLFvlRfJ4pvxK7FrXuGb5K41KB-g",
+                },
+            });
+        const data = await response.json();
+        console.log(data);
+        printProduct(data);
+    } catch (error) {
+        console.log(error);
+    };   
+}
+
+function printProduct(data) {
+    for (let i = 0; i < data.length; i++) {
+        div.innerHTML += `<div class="card col-3">
+        <img src="${data[i].imageUrl}" class="card-img-top">
+        <div class="card-body">
+            <h5 class="card-title">${data[i].name}</h5>
+            <h6 class="card-subtitle">${data[i].brand}</h6>
+            <p class="card-text">${data[i].description}</p>
+            <p class="card-text">${data[i].price}€</p>
+            <a href="#" class="btn btn-primary btnMod">Modifica</a>
+            <a href="#" class="btn btn-primary">Scopri di più</a>
+        </div>
+    </div>`;
+    };
+    const btnMod = document.querySelectorAll(".btnMod");
+    for (let i = 0; i < data.length; i++) {
+        btnMod[i].addEventListener("click", function() {
+            sessionStorage.setItem("id", data[i]._id);
+            location.href = "backOffice.html";
+        });
+    }
+}
